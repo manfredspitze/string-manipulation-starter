547a7970676321cd8bc0a32f3c90c97c9a0b03da
Manfred Spitze <155758012+manfredspitze@users.noreply.github.com>
2024-09-14 11:03:39 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,7 @@
 # Abraham Lincoln
-# 05 SEP 2024
-# Hello World 
+# 05 SEP 20XX
+# String Manipulation in Python 
 
-greeting = 'Good morning'
-first_name = 'Mikey'
-
-custom_greeting = f'{greeting}, {first_name}!'
-
-print(custom_greeting)
-print(f'By the way, what is your last name, {first_name}?')
 
 
 
